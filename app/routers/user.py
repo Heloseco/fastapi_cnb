229da8ad65d6678dfdb9e107c051693280c46793
ristonhen <ristonhen@gmail.com>
2023-09-16 11:42:58 +0700

    complete step 1

--- app/routers/user.py
+++ app/routers/user.py
@@ -24,6 +24,12 @@ def create_user(user: schemas.UserCreate,db: Session = Depends(get_db),
     db.refresh(new_user)
     return new_user
 
+@router.get("/", response_model=List[schemas.UserOut])
+def get_all_users(db: Session = Depends(get_db),
+                  current_user: int = Depends(oauth2.get_current_user)):
+    users = db.query(models.User).all()
+    return users
+
 @router.get("/{id}",response_model= schemas.UserOut)
 def get_user(id: int ,db: Session = Depends(get_db),
                 current_user: int = Depends(oauth2.get_current_user)):
